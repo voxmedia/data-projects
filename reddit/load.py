2ef36f15ceda2941ddfb15f1f67c0bcad1b739cc
Ryan Mark <ryan@mrk.cc>
2015-07-10 16:17:11 -0400

    hopefully loads faster

--- reddit/load.py
+++ reddit/load.py
@@ -10,25 +10,67 @@ if not os.path.exists(sys.argv[1]):
     sys.exit('ERROR: File %s was not found!' % sys.argv[1])
 
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 import json
 
 es = Elasticsearch()
 
 
-def handle_file(fp):
-    for line in fp:
-        comment = json.loads(line)
-        created = datetime.utcfromtimestamp(int(comment['created_utc']))
-        retreived = datetime.utcfromtimestamp(int(comment['retrieved_on']))
-        del comment['created_utc']
-        del comment['retrieved_on']
-        comment['created_utc'] = created
-        comment['retrieved_on'] = retreived
-        es.index(
-            index='reddit', doc_type='comment',
-            id=comment['id'], parent=comment['parent_id'],
-            body=comment)
+def actions(filename):
+    with open_file(filename) as fp:
+        for line in fp:
+            yield make_action(line)
+
+
+def make_action(json_string):
+    doc = parse_comment(json_string)
+    return {
+        '_index': 'reddit',
+        '_type': 'comment',
+        '_id': doc['id'],
+        '_parent': doc['parent_id'],
+        '_source': doc
+    }
+
+
+def parse_comment(json_string):
+    comment = json.loads(json_string)
+
+    created = datetime.utcfromtimestamp(int(comment['created_utc']))
+    retreived = datetime.utcfromtimestamp(int(comment['retrieved_on']))
+    del comment['created_utc']
+    del comment['retrieved_on']
+    comment['created_utc'] = created
+    comment['retrieved_on'] = retreived
+
+    return comment
+
+
+def open_file(filename):
+    if filename.endswith('.json'):
+        return open(filename)
+    elif filename.endswith('.bz2'):
+        from bz2 import BZ2File
+        return BZ2File(filename)
+
+
+def simple_load(filename):
+    with open_file(filename) as fp:
+        for line in fp:
+            comment = parse_comment(line)
+            es.index(
+                index='reddit', doc_type='comment',
+                id=comment['id'], parent=comment['parent_id'],
+                body=comment)
+
+
+def bulk_load(filename):
+    successes, errors = helpers.bulk(es, actions(filename))
+    print "Added %i documents" % successes
+    if len(errors) > 0:
+        print errors.join("\n")
+    else:
+        print 'No errors'
 
 
 def main():
@@ -39,13 +81,12 @@ def main():
 
     filename = sys.argv[1]
 
-    if filename.endswith('.json'):
-        with open(filename) as fp:
-            handle_file(fp)
-    elif filename.endswith('.bz2'):
-        from bz2 import BZ2File
-        with BZ2File(filename) as fp:
-            handle_file(fp)
+    print """
+
+Starting data import, this will take a long time...
+
+"""
+    bulk_load(filename)
 
 if __name__ == '__main__':
     main()
